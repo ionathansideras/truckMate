900d80a1ba4546f216bbee4473393ba93aea4e9d
myNameIsIon <iona8ansideras@gmail.com>
2024-02-06 22:22:15 +0200

    Refactor SiteOverview component and update styles

--- src/components/SiteOverview.tsx
+++ src/components/SiteOverview.tsx
@@ -1,3 +1,34 @@
+import { useState } from "react";
+import { overviewData } from "../data/overviewData";
+
+interface OverviewData {
+    id: string;
+    title: string;
+    text: string;
+    img: string;
+    alt: string;
+    elementId: string;
+    elementClass: string;
+}
+
 export default function SiteOverview() {
-    return <div className="overview"></div>;
+    return (
+        <div className="overview">
+            {overviewData.map((item: OverviewData) => {
+                return (
+                    <section
+                        key={item.id}
+                        id={item.elementId}
+                        className={item.elementClass}
+                    >
+                        <div className="overview-info">
+                            <h1>{item.title}</h1>
+                            <p>{item.text}</p>
+                        </div>
+                        <img src={item.img} alt={item.alt} />
+                    </section>
+                );
+            })}
+        </div>
+    );
 }
